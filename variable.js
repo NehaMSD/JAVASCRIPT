c929c9c1775d11facbf7c84ae9294e2775997b21
NehaMSD <nehams.21ece@kongu.edu>
2024-01-21 10:21:47 +0530

    neha

---
+++ variable.js
@@ -0,0 +1,8 @@
+a='a'
+console.log(a)
+var b= 'var'
+console.log("This variable belongs to var type",b)
+let c='let'
+console.log("This variable belongs to var type",c)
+const d=100
+console.log("This variable belongs to var type",d)
