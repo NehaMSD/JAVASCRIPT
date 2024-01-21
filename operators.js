0563d12e76f2331897af7594f78d56db6b4d66c9
NehaMSD <nehams.21ece@kongu.edu>
2024-01-21 10:23:42 +0530

    neha

---
+++ operators.js
@@ -0,0 +1,34 @@
+
+//Assignment operator
+var a=10
+console.log("Assignment operator",a)
+
+//Arithmetic Operator
+var aA=10
+var bA=10
+console.log(typeof aA)
+console.log("Arithmetic Operator Addition Output",aA+bA)
+console.log("Arithmetic Operator Subtraction Output",aA-bA)
+console.log("Arithmetic Operator Multiplication Output",aA*bA)
+console.log("Arithmetic Operator Division Output",aA/bA)
+console.log("Arithmetic Operator Modulo Output",aA%bA)
+
+//Logical Operator &&,||,!
+var aA=10
+var bA=10
+console.log("Logical Operator && Output",aA&&bA)
+console.log("Logical Operator || Output",aA||bA)
+console.log("Logical Operator ! Output",!aA)
+
+console.log(10&&20)
+console.log(0&&20)
+console.log(20&&0)
+console.log(0&&(10||20))
+console.log(0&&10||20)
+
+console.log(10||20)
+console.log(0||20)
+console.log(20||0)
+console.log(100||20&&0)
+console.log((100||20)&&0)
+
