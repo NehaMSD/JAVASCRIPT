7f1d0e5dc2c7ef6cb9fce3c88aab5c52612ef98a
NehaMSD <nehams.21ece@kongu.edu>
2024-01-23 11:21:40 +0530

    neha

--- promises.js
+++ promises.js
@@ -12,11 +12,42 @@ function isAuthenticated() {
         }
       }, 1000);
     });
-  }
-  Login()
+}
+Login()
     .then(function (message) {
       console.log(message);
     })
     .catch(function (message) {
       console.log(message);
     });
+
+//PROMISE
+//location eg
+function isLocationValid(loc) {
+    var validLocation = "Trichy";
+    return loc === validLocation;
+    }
+      
+    function location() {
+        return new Promise((resolve, reject)=>{
+          setTimeout(function () {
+            var loc = "Trichy";
+            console.log(loc);
+            if (isLocationValid(loc)) {
+              resolve("Location found");
+            } else {
+              reject("Location not found");
+            }
+        }, 2000);
+    });
+}
+      
+location()
+    .then((response) =>{
+    console.log(response);
+    })
+    .catch((errorMessage) =>{
+        console.log(errorMessage);
+    });
+    
+
