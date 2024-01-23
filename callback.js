e32ebbdbed25f7bdaefc3ec0aafcab743584a5ed
NehaMSD <nehams.21ece@kongu.edu>
2024-01-23 11:18:08 +0530

    neha

--- callback.js
+++ callback.js
@@ -1,11 +1,27 @@
+//callback
 function ramuSomu(WelcomeMessage,callback){ //callback may be any name.
     setTimeout(function(){
         console.log(WelcomeMessage)
         callback();
     },2000)
-
 }
+
 function miniani(){
     console.log("don't come")
 }
+
 ramuSomu("Hello ramuSomu \nwelcome to kec",miniani)
+
+//form submission example
+function submitForm(buttonClick,db){
+    setTimeout(function(){
+        console.log(buttonClick)
+        db()
+    },2000)
+}
+
+function database(){ //function is passed to object
+    console.log("Form submitted successfully")
+}
+
+submitForm("Form submit button clicked",database)
